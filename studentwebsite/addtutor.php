9fb07885ca97158a30105e0fb706509755a5cbc6
17721250181 <60857118+17721250181@users.noreply.github.com>
2022-02-24 12:07:01 +1300

    Added erroe checking

--- studentwebsite/addtutor.php
+++ studentwebsite/addtutor.php
@@ -1,19 +1,38 @@
-<div class="row m-5">
-  <div class="col-12">
-    <form class="" action="entertutor.php" method="post">
-      <div class="mb-3">
-        <label for="tutorname" class="form-label">Tutor Name</label>
-        <input type="text" class="form-control required" id="tutorname" aria-describedby="tutorname">
+<div class="container-fluid">
+<?php
+  // To know did error happened
+  if (isset($_GET['tutorexisterror'])) {
+    // $tutorexisterror=$_GET['tutorexisterror'];
+    ?>
+    <div class="row mx-5 mt-4">
+      <div class="col-12">
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <i class="bi bi-exclamation-triangle"></i>
+          <strong>Gosh!</strong> You have put a tutor who is already exist.
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
       </div>
-      <div class="mb-3">
-        <label for="tutorcode" class="form-label">Tutor Code</label>
-        <input type="text" class="form-control required" id="tutorcode">
-      </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">I am a robot</label>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </div>
+    <?php
+  }
+ ?>
+  <div class="row my-4 mx-5">
+    <div class="col-12">
+      <form class="" action="entertutor.php" method="post">
+        <div class="mb-3">
+          <label for="tutorname" class="form-label">Tutor Name</label>
+          <input name="tutorname" type="text" class="form-control" id="tutorname" aria-describedby="tutorname" required>
+        </div>
+        <div class="mb-3">
+          <label for="tutorcode" class="form-label">Tutor Code</label>
+          <input name="tutorcode" type="text" class="form-control" id="tutorcode" required>
+        </div>
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">I am a robot</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
   </div>
 </div>
--- studentwebsite/entertutor.php
+++ studentwebsite/entertutor.php
@@ -4,9 +4,20 @@
   if (!isset($_POST['tutorname']) or !isset($_POST['tutorcode'])) {
     header("Location:index.php?page=addtutor");
   }
-  $tutorname=$_POST['tutorname'];
-  $tutorcode=$_POST['tutorcode'];
+  else {
+    $tutorname=$_POST['tutorname'];
+    $tutorcode=strtoupper($_POST['tutorcode']);
+    // Check is name/code already exist
+    $mysqli_sql = "SELECT * FROM `tutorgroup` WHERE tutor='$tutorname' OR tutorcode='$tutorcode'";
+    $mysqli_qry = mysqli_query($dbconnect, $mysqli_sql);
+    if (mysqli_num_rows($mysqli_qry)!=0) {
+      header("Location:index.php?page=addtutor&tutorexisterror=1");
+    }
+    else {
+      $mysqli_sql = "INSERT INTO tutorgroup (tutor, tutorcode) VALUES ('$tutorname', '$tutorcode')";
+      echo $mysqli_query;
+      mysqli_query($dbconnect, $mysqli_sql);
+    }
+  }
 
-  $mysqli_query = "INSERT INTO tutorgroup (tutor, tutorcode) VALUES ('$tutorname', '$tutorcode')";
-  mysqli_query($dbconncet, $mysqli_query);
  ?>
